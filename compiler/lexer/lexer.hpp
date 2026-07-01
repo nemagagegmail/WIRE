ac6dfaa186a3f134cc08b031c88614c942c758ee
nemagagegmail <nemagage.y@gmail.com>
2026-07-01 09:07:08 +0300

    Add lexer.hpp with TokenType and Token struct

---
+++ compiler/lexer/lexer.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <string>
+#include <cctype>
+#include <vector>
+
+namespace WIRE {
+
+enum class TokenType
+  TOKEN_NONE,
+  TOKEN_RETURN,
+
+  
+  TOKEN_PUSH,
+  TOKEN_POP,
+  TOKEN_ADD,
+  TOKEN_SUB,
+  TOKEN_MUL,
+  TOKEN_DIV,
+  TOKEN_POW,
+  TOKEN_ROT,
+
+  TOKEN_CLAMP,
+
+  TOKEN_VME
+};
+
+struct Token {
+  TokenType; // Write here the type from the list above 
+  std::string value; // Here we write the text itself, like "+" or "123" 
+};
+
+} // Closing the namespace
